9765d75d4d5b77669cffef8e089f62c2cb3fddc3
anega <anegamega@gmail.com>
2021-10-27 21:32:51 +0300

    Add MoviesScreenWidget sceleton.

---
+++ lib/widgets/movies_screen/movies_screen_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MoviesScreenWidget extends StatelessWidget {
+  const MoviesScreenWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(itemBuilder: (BuildContext context, int index) {
+      return Center();
+    });
+  }
+}
