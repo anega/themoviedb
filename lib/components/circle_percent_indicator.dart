8eaa2004d2ea77593f5466401ea40d7ddcf2fcbc
anega <anegamega@gmail.com>
2021-11-01 17:04:01 +0200

    Add background circle for the progress indicator.

--- lib/components/circle_percent_indicator.dart
+++ lib/components/circle_percent_indicator.dart
@@ -16,7 +16,11 @@ class CirclePercentIndicator extends StatelessWidget {
 
 class CustomCircleIndicator extends CustomPainter {
   @override
-  void paint(Canvas canvas, Size size) {}
+  void paint(Canvas canvas, Size size) {
+    var bgCirclePaint = Paint();
+    bgCirclePaint.color = Color(0xFF081C22);
+    canvas.drawCircle(Offset(50, 50), 50.0, bgCirclePaint);
+  }
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
