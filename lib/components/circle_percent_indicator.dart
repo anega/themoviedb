5c7a336e5acfdb133db8f1719fb2dc007558e01e
anega <anegamega@gmail.com>
2021-11-01 20:39:57 +0200

    Circle progress indicator widget for Movie page.

--- lib/components/circle_percent_indicator.dart
+++ lib/components/circle_percent_indicator.dart
@@ -2,41 +2,108 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 
+/* usage
+
+        CirclePercentIndicator(
+          indicatorRadius: 50,
+          percent: 0.01,
+          circleBgColor: Color(0xFF081C22),
+          percentDoneColor: Color(0xFFD2D531),
+          percentLeftColor: Color(0xFF423D0F),
+        ),
+ */
+
 class CirclePercentIndicator extends StatelessWidget {
+  final double indicatorRadius;
+  final double percent;
+  final Color circleBgColor;
+  final Color percentDoneColor;
+  final Color percentLeftColor;
+
+  const CirclePercentIndicator(
+      {required this.indicatorRadius,
+      required this.percent,
+      required this.circleBgColor,
+      required this.percentDoneColor,
+      required this.percentLeftColor});
+
   @override
   Widget build(BuildContext context) {
+    final int percentValueToShow = (percent * 100).toInt();
+
     return Container(
-      width: 100.0,
-      height: 100.0,
-      decoration: BoxDecoration(border: Border.all(color: Colors.red)),
-      child: CustomPaint(
-        painter: CustomCircleIndicator(),
+      width: indicatorRadius * 2,
+      height: indicatorRadius * 2,
+      child: Stack(
+        fit: StackFit.expand,
+        children: [
+          CustomPaint(
+            painter: CirclePercentIndicatorPainter(
+              indicatorRadius: indicatorRadius,
+              percent: percent,
+              circleBgColor: circleBgColor,
+              percentDoneColor: percentDoneColor,
+              percentLeftColor: percentLeftColor,
+            ),
+          ),
+          Center(
+            child: Text(
+              '$percentValueToShow%',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
 }
 
-class CustomCircleIndicator extends CustomPainter {
+class CirclePercentIndicatorPainter extends CustomPainter {
+  final double indicatorRadius;
+  final double percent;
+  final Color circleBgColor;
+  final Color percentDoneColor;
+  final Color percentLeftColor;
+
+  CirclePercentIndicatorPainter(
+      {required this.indicatorRadius,
+      required this.percent,
+      required this.circleBgColor,
+      required this.percentDoneColor,
+      required this.percentLeftColor});
+
   @override
   void paint(Canvas canvas, Size size) {
     var bgCirclePaint = Paint();
-    bgCirclePaint.color = Color(0xFF081C22);
-    canvas.drawCircle(Offset(50, 50), 50.0, bgCirclePaint);
+    bgCirclePaint.color = circleBgColor;
+    canvas.drawOval(Offset.zero & size, bgCirclePaint);
 
     var percentLeft = Paint();
-    percentLeft.color = Color(0xFF423D0F);
+    percentLeft.color = percentLeftColor;
     percentLeft.style = PaintingStyle.stroke;
     percentLeft.strokeWidth = 4;
-    percentLeft.strokeCap = StrokeCap.round;
     canvas.drawArc(
-        Offset.zero & Size(100.0, 100.0), 0 - pi, 2 * pi, false, percentLeft);
+      Offset(4, 4) & Size(size.width - 8, size.height - 8),
+      pi * 2 * percent - (pi / 2),
+      pi * 2 * (1.0 - percent),
+      false,
+      percentLeft,
+    );
 
-    var percentDone = Paint();
-    percentDone.color = Color(0xFFD2D531);
+    final percentDone = Paint();
+    percentDone.color = percentDoneColor;
     percentDone.style = PaintingStyle.stroke;
     percentDone.strokeWidth = 4;
     percentDone.strokeCap = StrokeCap.round;
-    canvas.drawArc(Offset.zero & Size(100.0, 100.0), 0, pi, false, percentDone);
+    canvas.drawArc(
+      Offset(4, 4) & Size(size.width - 8, size.height - 8),
+      -pi / 2,
+      pi * 2 * percent,
+      false,
+      percentDone,
+    );
   }
 
   @override
